707ce78e0d48756f85fbc6e870344b02fb13ebb8
KantTurnsInHisGrave <112070854+KantTurnsInHisGrave@users.noreply.github.com>
2022-10-29 14:16:57 +0100

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,4 +2,5 @@
 
 cd /home/jenkins/.jenkins/workspace/DFE_Final_Project_Pipelinev2
 
-sudo docker ps -q --filter "name=myapp" | sudo grep -q; sudo docker stop myapp; sudo docker rm myapp; sudo docker rmi myapp
+
+sudo docker stop myapp; sudo docker rm myapp; sudo docker rmi myapp
